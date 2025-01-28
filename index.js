66209b61ced958764d87d41277d74938d5f79106
adnanroshan <adnanroshan46@gmail.com>
2025-01-27 23:06:28 -0500

    database connected

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+
+const port = process.env.PORT || 3002
+const app = express();
+
+connectDB();
+
+app.listen(port,()=>{
+    console.log(`port running on ${port}`);
+    
+});
